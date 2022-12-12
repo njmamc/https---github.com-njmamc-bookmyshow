06166378cd612ad4fdc014ed4e39b61734e58e9c
njmamc <109453019+njmamc@users.noreply.github.com>
2022-12-12 23:22:10 +0530

    logo changes

--- bookMyShow/bookmyshow/src/components/Header/Header.js
+++ bookMyShow/bookmyshow/src/components/Header/Header.js
@@ -6,12 +6,20 @@ const Header = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbarBGcolor">
                 <div className="container-fluid hi">
-                    <img className='bookMyshowLogo' src="../public/Raw Data/Images/bookmyshowLogo.png" alt="" />
+                    <img className='bookMyshowLogo' src="bookmyshowLogo.png" alt="" />
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        
+                        <label htmlFor="searchIcon" className='searchIcon'>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+
+                        </label>
+                        <input className="form-control me-2 searchBar" type="search" placeholder="Search for Movies, Events, Plays, Sports and Activities" aria-label="Search" />
 
                     </form>
                     <div className="collapse navbar-collapse headerSignIn" id="navbarSupportedContent">
@@ -45,10 +53,10 @@ const Header = () => {
 
 
                     <nav className='RightSideLinks_1'>
-                    <a className="nav-link RightSideLinks" href="#">ListYourShow</a>
-                    <a className="nav-link RightSideLinks" href="#">Corporates</a>
-                    <a className="nav-link RightSideLinks" href="#">Offers</a>
-                    <a className="nav-link RightSideLinks" href="#">Gift Cards</a>
+                        <a className="nav-link RightSideLinks" href="#">ListYourShow</a>
+                        <a className="nav-link RightSideLinks" href="#">Corporates</a>
+                        <a className="nav-link RightSideLinks" href="#">Offers</a>
+                        <a className="nav-link RightSideLinks" href="#">Gift Cards</a>
                     </nav>
 
                 </nav>
