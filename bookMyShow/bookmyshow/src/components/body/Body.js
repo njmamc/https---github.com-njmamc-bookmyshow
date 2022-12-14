d2ff298296f575d83b0587b3b31c282f5bed6092
njmamc <109453019+njmamc@users.noreply.github.com>
2022-12-14 18:47:49 +0530

    title change

--- bookMyShow/bookmyshow/src/components/body/Body.js
+++ bookMyShow/bookmyshow/src/components/body/Body.js
@@ -6,17 +6,47 @@ import React from 'react'
 const Body = () => {
   return (
     <>
-        <h4 className='recomendedMoviesText'>Recommended Movies</h4>
-        <div className='imagesOfRecommendation'>
-            <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@like_202006280402.png,ox-24,oy-617,ow-29:ote-NTkwLjJLIGxpa2Vz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00037264-zzvtkurznk-portrait.jpg" alt="" />
-            <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC45LzEwICA5Mi4zSyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00331997-ptqfdfmgtr-portrait.jpg" alt="" />
-            <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny45LzEwICAzMi42SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00312739-twjrkftutt-portrait.jpg" alt="" />
-            <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC40LzEwICAxNi41SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00327173-yaevebvyah-portrait.jpg" alt="" />
-            <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC4yLzEwICAxLjNLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00341647-jfyallvcub-portrait.jpg" alt="" />
+      <h4 className='recomendedMoviesText'>Recommended Movies</h4>
+      <div className='imagesOfRecommendation'>
+        <div className='flexDirectionRecomed'>
+          <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@like_202006280402.png,ox-24,oy-617,ow-29:ote-NTkwLjJLIGxpa2Vz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00037264-zzvtkurznk-portrait.jpg" alt="" />
+          <label className='labelforRecommedation_title' htmlFor="/">Avatar: The Way of Water</label>
+          <label className='labelforRecommedation_category' htmlFor="/">Action/Adventure/Fantasy/Sci-Fi</label>
         </div>
-    
-    </>
-  )
+
+        <div className='flexDirectionRecomed'>
+          <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC45LzEwICA5Mi4zSyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00331997-ptqfdfmgtr-portrait.jpg" alt="" />
+          <label className='labelforRecommedation_title' htmlFor="/">Drishyam 2</label>
+          <label className='labelforRecommedation_category' htmlFor="/">Drama/Mystery/Thriller</label>
+          
+        </div>
+
+        <div className='flexDirectionRecomed'>
+          <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny45LzEwICAzMi42SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00312739-twjrkftutt-portrait.jpg" alt="" />
+          <label className='labelforRecommedation_title' htmlFor="/">Bhediya</label>
+          <label className='labelforRecommedation_category' htmlFor="/">Comedy/Thriller</label>
+        </div>
+
+        <div className='flexDirectionRecomed'>
+          <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC40LzEwICAxNi41SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00327173-yaevebvyah-portrait.jpg" alt="" />
+          <label className='labelforRecommedation_title' htmlFor="/">An Action Hero</label>
+          <label className='labelforRecommedation_category' htmlFor="/">Action/Comedy/Thrillers</label>
+        </div>
+
+        <div className='flexDirectionRecomed'>
+          <img className='imgforRecomendTo' src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC4yLzEwICAxLjNLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00341647-jfyallvcub-portrait.jpg" alt="" />
+          <label className='labelforRecommedation_title' htmlFor="/">Salaam Venky</label>
+          <label className='labelforRecommedation_category' htmlFor="/">Drama/Family</label>
+          </div>
+          </div>
+
+
+          <section>
+          <img className='adAfterRecommendation' src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120:q-80/stream-leadin-web-collection-202210241242.png" alt="" />
+          </section>
+
+        </>
+        )
 }
 
-export default Body
+        export default Body
